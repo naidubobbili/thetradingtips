957a4af241dafe343b6bd4767c1e20beb4f75d90
naidubobbili <58201922+naidubobbili@users.noreply.github.com>
2020-08-26 21:24:15 +0530

    Update models.py

--- website/models.py
+++ website/models.py
@@ -8,7 +8,7 @@ class topic(models.Model):
     Title=models.CharField(default='TheTradingTips',max_length=200)
     Text=models.TextField(default='TheTradingTips')
     #Pic=models.ImageField(default='Cover_Photos/tradingtips.jpg',upload_to='Cover_Photos')
-    Pic=models.CharField(default='https://1.bp.blogspot.com/-R4caXMiOVOU/X0WbF5oOnTI/AAAAAAAAAlw/QPyd7ZD7zSMODp5cI5knre1rJamDmwl3wCNcBGAsYHQ/s640/tradingtips.jpg',max_length=250)
+    Pic=models.CharField(default='https://bit.ly/34zqnFW',max_length=250)
     #DetailPagePic=models.ImageField(default='DetailPage.jpg/DetailPagePhotos',upload_to='DetailPagePhotos')
     #uploaded_on=models.TextField(default='TheTradingTips')
 
